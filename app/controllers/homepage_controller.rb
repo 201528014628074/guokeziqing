1d541d9e00b8fecb6f2f152c49d452e1f0e76b66
wcyy <975706314@qq.com>
2016-01-10 19:37:16 +0800

    guokeziqing-git

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,9 +2,15 @@ class HomepageController < ApplicationController
   def index
     @user_id = params[:id] || session[:user_id]
     session[:user_id] = @user_id
-    #@user_id = 1
+    @searchwords = params[:searchwords] || session[:searchwords] || ''
+    session[:searchwords] = @searchwords
     @users = UserInfo.where(:user_login_id => @user_id)
-    @all_posts = Post.order(:edit_time).all
+    if @searchwords == ''
+      @all_posts = Post.order(:edit_time).where(:is_notice => false)
+    else
+      sql_string = 'select * from posts where title LIKE \'%' + @searchwords + '%\''
+      @all_posts = Post.order(:edit_time).find_by_sql(sql_string)
+    end
     @posts_content = {}
     @groups = {}
     @likes_nums = {}
@@ -12,6 +18,9 @@ class HomepageController < ApplicationController
     @comments_nums = {}
     @pro_root = Rails.root.to_s
     @all_posts.each do |post|
+      if post.is_notice == true
+        next
+      end
       f = File.open(@pro_root + post.context_url, 'r')
       @posts_content[post.id] = f.gets
       @groups[post.id] = GroupInfo.find(post.group_info_id).group_name
@@ -19,10 +28,6 @@ class HomepageController < ApplicationController
       @collect_nums[post.id] = Collection.where(:post_id => post.id).count
       @comments_nums[post.id] = Comment.where(:post_id => post.id).count
     end
-    #@all_posts.each do |post|
-      #f = File.open('#{RAILS_ROOT}/app/assets/data/posts/1.txt', 'r')
-      #@posts_content.push(f.gets)
-    #end
     @user = @users[0]
     @user_info = {}
     @user_info['nick_name'] = @user.nick_name
@@ -30,9 +35,41 @@ class HomepageController < ApplicationController
     @user_info['home'] = @user.home_provice + '    ' + @user.home_city
     @user_info['institute'] = Institute.find(@user.institute_id).inst_name
     @user_info['email'] = @user.email
-    #@nick_name = user.nick_name
-    #@gender = user.gender
-    #@home = user.home_provice + '\t' + user.home_city
-    #@institute = Institute.where(:id => user.institute_id)
+    get_hot_posts
+  end
+
+  def get_hot_posts
+    @hot_posts = {}
+    @hot_posts_id = Array.new
+    @hot_posts_title = {}
+    posts_score = Array.new
+    posts_id = Array.new
+    posts_title = {}
+    posts = Post.all
+    posts.each do |post|
+      if post.is_notice == true
+        next
+      end
+      end_time = Time.now
+      start_time = Time.new(post.edit_time.to_s)
+      time_diff = (end_time - start_time)/3600/24
+      if time_diff < 11
+        likes_nums = LikePost.where(:post_id => post.id).count
+        collect_nums = Collection.where(:post_id => post.id).count
+        comments_nums = Comment.where(:post_id => post.id).count
+        post_score = likes_nums + collect_nums + comments_nums + 30.0/(time_diff + 1)
+        posts_score.push(post_score)
+        posts_id.push(post.id)
+        posts_title[post.id] = post.title
+      end
+    end
+    for i in 0..5
+      max_index = posts_score.index posts_score.max
+      @hot_posts[posts_id[max_index]] = posts_score[max_index]
+      @hot_posts_id.push(posts_id[max_index])
+      @hot_posts_title[posts_id[max_index]] = posts_title[posts_id[max_index]]
+      posts_score.delete_at(max_index)
+      posts_id.delete_at(max_index)
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,21 @@ group2 = GroupInfo.create(:group_name => '随机过程', :user_login_id => user2
 
 post1 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容', :context_url => '/app/assets/data/posts/1.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
 post2 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容', :context_url => '/app/assets/data/posts/2.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+post3 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容1', :context_url => '/app/assets/data/posts/3.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
+post4 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容1', :context_url => '/app/assets/data/posts/4.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+post5 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容2', :context_url => '/app/assets/data/posts/5.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
+post6 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容2', :context_url => '/app/assets/data/posts/6.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+post7 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容3', :context_url => '/app/assets/data/posts/7.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
+post8 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容3', :context_url => '/app/assets/data/posts/8.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+post9 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容4', :context_url => '/app/assets/data/posts/9.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
+post10 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容4', :context_url => '/app/assets/data/posts/10.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+#post11 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容5', :context_url => '/app/assets/data/posts/11.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
+#post12 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容5', :context_url => '/app/assets/data/posts/12.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+#post13 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容6', :context_url => '/app/assets/data/posts/13.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
+#post14 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容6', :context_url => '/app/assets/data/posts/14.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+#post15 = Post.create(:group_info_id => group1.id, :title => '羽毛球考试内容7', :context_url => '/app/assets/data/posts/15.txt', :user_login_id => user2.id, :edit_time => Time.new, :is_notice => false)
+#post16 = Post.create(:group_info_id => group2.id, :title => '随机过程考试内容7', :context_url => 's/app/assets/data/posts/16.txt', :user_login_id => user1.id, :edit_time => Time.new, :is_notice => false)
+
 
 comment1 = Comment.create(:post_id => post1.id, :edit_time => Time.new, :context_url => '#{RAILS_ROOT}/app/assets/data/comments/1.txt', :user_login_id => user1.id)
 comment2 = Comment.create(:post_id => post2.id, :edit_time => Time.new, :context_url => '#{RAILS_ROOT}/app/assets/data/comments/2.txt', :user_login_id => user2.id)
